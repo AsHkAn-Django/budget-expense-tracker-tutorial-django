c14a870c5c875592d5b02ecd2f1f324da01e4f5c
AsHkaN <ashkan_ahrari@yahoo.com>
2025-04-28 13:50:49 +0300

    task added

--- myApp/tasks.py
+++ myApp/tasks.py
@@ -1,5 +1,12 @@
 from celery import shared_task
+from django.core.mail import send_mail
 
 @shared_task
-def add(x, y):
-    return x + y
+def add(categ, user):
+    subject = f"Spend Alert!"
+    message = f"Your category '{categ}' overreached the limit! "
+    send_mail(subject=subject,
+              message=message,
+              from_email=None,
+              recipient_list=[user.email])
+    
